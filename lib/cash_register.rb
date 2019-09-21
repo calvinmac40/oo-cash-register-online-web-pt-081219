f083b24c5b97bc673af27e49b5effad7ddfe38ad
Calvin McDowell <calvinmac40@hotmail.com>
2019-09-21 19:13:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,9 +12,9 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    @total  
-    @price = price
-    self.total += price * quantity
+    @total += price * quantity
+    quantity.times do
+      items << title 
   end
   
   def apply_discount
