de09f52142842716368b2799e97fe55415f14f66
Calvin McDowell <calvinmac40@hotmail.com>
2019-09-21 18:05:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :add_item
+  
+  def initialize(total = 0)
+    @total = total
+    @discount = 20
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(title, price, quantity)
+    @title = title 
+    @price = price
+    self.quantity
+    self.total += price
+    
+  end
+end
